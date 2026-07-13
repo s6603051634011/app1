597751adf948cef5a3e77b242aeff94f841c77dc
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-07-13 11:15:41 +0700

    Add style.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
-  const divStyle={
-    color: 'red' ,
-    backgroundColor:'powderblue',
-    fontSize:'larger',
-    padding:'3px'
-  }
-  return <div style={divStyle}>Hello React</div>
+  
+  return (
+    <>
+      <h2>Developer Thai.com</h2>
+      <div className='title'>React & React Native</div>
+    </>
+  )
 }
 
 export default App;
