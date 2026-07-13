9d9f908b0c7427fd4747aeab72f6b3309900fefb
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-07-13 11:05:04 +0700

    Update App.js with css inline

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let r = 'React'
-  let rn = 'React Native'
-  const el=(
-      <div>
-        Create Web & Mobile Apps<br/>
-        with {r} & {rn}
-      </div>
-    )
-  return el 
+  const divStyle={
+    color: 'red' ,
+    backgroundColor:'powderblue',
+    fontSize:'larger',
+    padding:'3px'
+  }
+  return <div style={divStyle}>Hello React</div>
 }
 
 export default App;
