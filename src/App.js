a595c354a097509efba4e5655e95f75972936482
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-07-20 11:24:51 +0700

    class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Content, Footer, Header } from "./func-components";
+import Calendar from "./class-components";
 
 function App() {
   
   return (
     <>
       <Header/>
+      <p><center><Calendar/></center></p>
       <Content/>
       <Footer/>
     </>
