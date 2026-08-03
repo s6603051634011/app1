5a62812077d1d080a46fe41b5c150d488091d908
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-08-03 10:46:50 +0700

    add EventData

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Content, Footer, Header } from "./func-components";
 import Calendar from "./class-components";
 import { Calculator2 } from "./calculator";
+import { EventData2 } from "./event-data";
 
 function App() {
   
-  return <Calculator2/>
+  return <EventData2/>
 }
 
 export default App;
