44e1049482b2102bf4337102d25f69f4f46f039c
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-08-10 11:19:52 +0700

    add State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { Content, Footer, Header } from "./func-components";
-import Calendar from "./class-components";
-import { Calculator2 } from "./calculator";
-import { EventData2 } from "./event-data";
-import RefsFunc from "./refs-func";
-import RefsArray from "./refs-array";
+import MessageBox from "./state-func";
 
 function App() {
   
-  return <RefsArray/>
+  return <MessageBox/>
 }
 
 export default App;
