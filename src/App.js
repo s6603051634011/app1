ce960c2e1bea85e697deb800f7f8f42faf699276
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-08-10 09:56:30 +0700

    add RefsFunc

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Content, Footer, Header } from "./func-components";
 import Calendar from "./class-components";
 import { Calculator2 } from "./calculator";
 import { EventData2 } from "./event-data";
+import RefsFunc from "./refs-func";
 
 function App() {
   
-  return <EventData2/>
+  return <RefsFunc/>
 }
 
 export default App;
