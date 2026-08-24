89125fd548f659bb84a6bf2ec837aed3b4e823e9
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-08-24 10:05:26 +0700

    Add effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import MessageBox from "./state-func";
+import Effect from "./effect";
 
 function App() {
   
-  return <MessageBox/>
+  return <Effect/>
 }
 
 export default App;
