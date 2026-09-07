99c6b8e80d93111b9e29f9718f084cfd78fa57e1
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-09-07 10:48:35 +0700

    Add Route
    use BrowserRouter, NavLink, Route, Routes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from "react";
 import {userContext} from './context'
 import Header2 from "./context-header2";
 import Content2 from "./context-content2";
+import Router2 from "./router2";
+import Router3 from "./router3";
 
-// function App() {
+
+function App() {
   
-//   return <Header2/>
-// }
-
-
-// export default App;
-
-export default function App() {
-  let [user, setUser] = React.useState()
-  return (
-    <userContext.Provider value={[user, setUser]}>
-      <Header2/>
-      <Content2/>
-    </userContext.Provider>
-  )
+  return <Router3/>
 }
+
+
+export default App;
+
+// export default function App() {
+//   let [user, setUser] = React.useState()
+//   return (
+//     <userContext.Provider value={[user, setUser]}>
+//       <Header2/>
+//       <Content2/>
+//     </userContext.Provider>
+//   )
+// }
