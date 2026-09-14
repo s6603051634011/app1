4e613d772410dc87db77baa3d9cdc83e19532405
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-09-14 10:22:27 +0700

    textarea

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Header2 from "./context-header2";
 import Content2 from "./context-content2";
 import Router2 from "./router2";
 import Router3 from "./router3";
+import FormText from "./form-text";
 
 
 function App() {
   
-  return <Router3/>
+  return <FormText/>
 }
 
 
