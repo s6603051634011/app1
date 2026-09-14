56fa2a9f5a5b4a78d0724d038005268ee9495ccd
s6603051634011 <s6603051634011@email.kmutnb.ac.th>
2026-09-14 10:53:40 +0700

    file

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import Content2 from "./context-content2";
 import Router2 from "./router2";
 import Router3 from "./router3";
 import FormText from "./form-text";
+import FormSelectFile from "./form-select-file";
 
 
 function App() {
   
-  return <FormText/>
+  return <FormSelectFile/>
 }
 
 
